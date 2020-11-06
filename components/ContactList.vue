7319eaeb062ea99981290a0fea4ba961346e4052
aaronmyatt <aaronmyatt@gmail.com>
2020-11-06 23:02:36 +0800

    extract contact list component

---
+++ components/ContactList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="contact in contacts" :key="contact.id">
+        {{ contact.id }} | {{ contact.firstName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async created() {
+    await this.$models.Contact.generateMock();
+    this.contacts = await this.$services.contactService.fetchAll();
+  },
+  data() {
+    return {
+      contacts: [],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
