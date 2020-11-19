3be68f6abb970676ece31f206058aca6791205f1
aaronmyatt <aaronmyatt@gmail.com>
2020-11-19 15:19:54 +0800

    drop purge

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,6 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    'nuxt-purgecss',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
