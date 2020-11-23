eb8ee58a925526e0253c46dcb6b777ac95bd058e
aaronmyatt <aaronmyatt@gmail.com>
2020-11-23 16:04:48 +0800

    prevent enter key submission within  textarea

--- mixins/EnterPressed.vue
+++ mixins/EnterPressed.vue
@@ -1,13 +1,14 @@
 <script>
 export default {
   mounted() {
-    window.addEventListener("keyup", this.listenForEnter);
+    this.$el.addEventListener("keyup", this.listenForEnter);
   },
   destroyed() {
-    window.removeEventListener("keyup", this.listenForEnter);
+    this.$el.removeEventListener("keyup", this.listenForEnter);
   },
   methods: {
     listenForEnter(e) {
+      if(e.target.tagName === 'TEXTAREA') return;
       if (e.key === "Enter") {
         this.enterPressedCallback();
       }
