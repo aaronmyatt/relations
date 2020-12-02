de73a228ba626aa4b246fcc5ae5fc7b9a5884542
aaronmyatt <aaronmyatt@gmail.com>
2020-12-02 22:20:37 +0800

    make index page lists collapsable

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,51 @@
   <div class="container">
     <div class="container mt-2 columns">
       <div class="column is-one-third">
-        <h2 class="is-size-2">Contacts</h2>
-        <b-button
-          icon-left="account-plus"
-          class="is-success mb-2"
-          expanded
-          @click="openContactFormModal"
+        <span
+          @click="showContactList = !showContactList"
+          class="is-flex is-align-items-center is-clickable"
         >
-          Add
-        </b-button>
-        <ContactList />
+          <h2 class="is-size-2">Contacts</h2>
+          <b-icon v-if="showContactList" size="is-large" icon="menu-down" />
+          <b-icon v-else size="is-large" icon="menu-right" />
+        </span>
+        <div v-if="showContactList">
+          <b-button
+            icon-left="account-plus"
+            class="is-success mb-2"
+            expanded
+            @click="openContactFormModal"
+          >
+            Add
+          </b-button>
+          <ContactList />
+        </div>
       </div>
       <div class="column is-one-third">
-        <h2 class="is-size-2">Encounters</h2>
-        <EncounterList />
+        <span
+          @click="showEncounterList = !showEncounterList"
+          class="is-flex is-align-items-center is-clickable"
+        >
+          <h2 class="is-size-2">Encounters</h2>
+          <b-icon v-if="showEncounterList" size="is-large" icon="menu-down" />
+          <b-icon v-else size="is-large" icon="menu-right" />
+        </span>
+        <div v-if="showEncounterList">
+          <EncounterList />
+        </div>
       </div>
       <div class="column is-one-third">
-        <h2 class="is-size-2">Plans</h2>
-        <PlanList />
+        <span
+          @click="showPlanList = !showPlanList"
+          class="is-flex is-align-items-center is-clickable"
+        >
+          <h2 class="is-size-2">Plans</h2>
+          <b-icon v-if="showPlanList" size="is-large" icon="menu-down" />
+          <b-icon v-else size="is-large" icon="menu-right" />
+        </span>
+        <div v-if="showPlanList">
+          <PlanList />
+        </div>
       </div>
     </div>
   </div>
@@ -30,6 +57,13 @@ import AddContactModal from "@/mixins/AddContactModal";
 
 export default {
   mixins: [AddContactModal],
+  data() {
+    return {
+      showContactList: true,
+      showEncounterList: true,
+      showPlanList: true,
+    };
+  },
   async created() {
     const firstTimeLogin = await services.settingService.table
       .where("name")
