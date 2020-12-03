f2431c5fad858bd86a1d73a12eb406faa96abf53
aaronmyatt <aaronmyatt@gmail.com>
2020-12-03 21:24:31 +0800

    respect maxPlansPerDay

--- store/plans.js
+++ store/plans.js
@@ -1,14 +1,5 @@
 import { services, models } from '@aaronmyatt/relations-data-model';
 
-function nextWeek(date) {
-    let newDate = date || new Date();
-    return new Date(newDate.setDate(newDate.getDate() + 7));
-}
-
-function offsetDate(date, offset){
-    return new Date(date.setDate(date.getDate() + offset));
-}
-
 export const state = function () {
     return {
         all: []
@@ -23,11 +14,13 @@ export const mutations = {
 
 export const actions = {
     async save({ dispatch }, { contactId, when, sooner, later }) {
+        when = await doNotExceedMaxPlansPerDay(when);
         const plan = new models.Plan(contactId, when, sooner, later);
         await services.planService.addOne(plan);
         dispatch('load');
     },
     async edit({ dispatch }, plan) {
+        plan.when = await doNotExceedMaxPlansPerDay(plan.when);
         await services.planService.updateOne(plan);
         dispatch('load');
     },
@@ -48,20 +41,9 @@ export const actions = {
         });
     },
     async addSoonerFor({ dispatch }, { contact }) {
-        const numberOfFuturePlans = await services.planService.fetchFutureFor(contact).count();
-        if (numberOfFuturePlans > 0) {
-            // Do nothing since we already have a plan lined up for this contact
-        } else {
-            let nextDate = new Date();
-            const daysBetweenLastPlans = await services.planService.daysBetweenLastPlans(contact);
+        let nextDate = await determineNextPlanDate(contact, 4, delayDateOffset);
 
-            if (daysBetweenLastPlans > 1) {
-                const delayDateOffset = Math.round(daysBetweenLastPlans / 2);
-                nextDate = offsetDate(nextDate, delayDateOffset);
-            } else {
-                nextDate = offsetDate(nextDate, 4);
-            }
-
+        if (nextDate) {
             await dispatch('save', {
                 contactId: contact.id,
                 when: new Date(nextDate),
@@ -72,20 +54,9 @@ export const actions = {
         }
     },
     async addLaterFor({ dispatch }, { contact }) {
-        const numberOfFuturePlans = await services.planService.fetfetchFutureFor(contact).count();
-        if (numberOfFuturePlans > 0) {
-            // Do nothing since we already have a plan lined up for this contact
-        } else {
-            let nextDate = new Date();
-            const daysBetweenLastPlans = await services.planService.daysBetweenLastPlans(contact);
+        let nextDate = await determineNextPlanDate(contact, 14, extendedDateOffset);
 
-            if (daysBetweenLastPlans > 1) {
-                const extendedDateOffset = daysBetweenLastPlans*2;
-                nextDate = offsetDate(nextDate, extendedDateOffset);
-            } else {
-                nextDate = offsetDate(nextDate, 14);
-            }
-
+        if (nextDate) {
             await dispatch('save', {
                 contactId: contact.id,
                 when: new Date(nextDate),
@@ -96,3 +67,46 @@ export const actions = {
         }
     },
 };
+
+function nextWeek(date) {
+    let newDate = date || new Date();
+    return new Date(newDate.setDate(newDate.getDate() + 7));
+}
+
+function offsetDate(date, offset) {
+    return new Date(date.setDate(date.getDate() + offset));
+}
+
+const delayDateOffset = (days) => Math.round(days / 2);
+
+const extendedDateOffset = (days) => Math.round(days * 2);
+
+const minDaysBetweenPlans = async () => await services.settingService.table.where("name").equals("minDaysBetweenPlans").first();
+const maxPlansPerDay = async () => await services.settingService.table.where("name").equals("maxPlansPerDay").first();
+
+async function determineNextPlanDate(contact, defaultOffset, offsetFunction) {
+    const minDaysBetweenPlansSetting = await minDaysBetweenPlans();
+
+    const numberOfFuturePlans = await services.planService.fetchFutureFor(contact).count();
+    if (numberOfFuturePlans > 0) return false;
+    // Do nothing since we already have a plan lined up for this contact
+    let nextDate = new Date();
+    const daysBetweenLastPlans = await services.planService.daysBetweenLastPlans(contact);
+
+    if (daysBetweenLastPlans > minDaysBetweenPlansSetting.value) {
+        nextDate = offsetDate(nextDate, offsetFunction(daysBetweenLastPlans));
+    } else {
+        nextDate = offsetDate(nextDate, defaultOffset);
+    }
+    return nextDate;
+}
+
+async function doNotExceedMaxPlansPerDay(date){
+    const maxPlansPerDaySetting = await maxPlansPerDay();
+    const plansOnDay = await services.planService.fetchAllForDate(date).count();
+    if(plansOnDay >= maxPlansPerDaySetting.value){
+        return doNotExceedMaxPlansPerDay(offsetDate(date, 1));
+    }else{
+        return date;
+    }
+}
